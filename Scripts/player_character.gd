ab79394f428384607d34c9d463df22f43a8da964
FunnyName <emi.houdart@student.uclouvain.be>
2026-03-27 14:14:16 +0100

    added recoil to player and added empty teams node

--- Scripts/player_character.gd
+++ Scripts/player_character.gd
@@ -1,25 +1,19 @@
-extends CharacterBody2D
+extends RigidBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
+@export var color :Color
+@export var impulse_strength := 200
+	
+var direction : Vector2
+func _ready() -> void:
+	$Sprite2D.modulate = color 
+  
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	look_at(get_local_mouse_position())
+	
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		shoot()      
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+func shoot():
+	var force_direction := (global_position - get_global_mouse_position()).normalized()
+	$".".apply_central_impulse( force_direction * impulse_strength)

98a439fd1df42fbce5a3689d0a7f39f8d950e2db
FunnyName <emi.houdart@student.uclouvain.be>
2026-03-27 14:53:47 +0100

    added shoot timer

--- Scripts/player_character.gd
+++ Scripts/player_character.gd
@@ -3,7 +3,10 @@ extends RigidBody2D
 @export var color :Color
 @export var impulse_strength := 200
 
+@onready var timer: Timer = $Timer
+
 var radius := 100 
+var canShoot := true
 
 func squareVect2(vector: Vector2):
 	#print(vector.x) 
@@ -16,8 +19,13 @@ func _physics_process(delta: float) -> void:
 	$eyes.position = get_local_mouse_position().normalized() * radius
 	
 	if Input.is_action_just_pressed("ui_accept"):
-		shoot()
+		if canShoot: shoot()
 
 func shoot():
 	var force_direction := (global_position - get_global_mouse_position()).normalized()
 	apply_central_impulse( force_direction * impulse_strength)
+	canShoot = false
+	timer.start()
+
+func _on_timer_timeout() -> void:
+	canShoot = true
